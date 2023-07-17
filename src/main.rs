0774a5a571def662c746be2f4f6f8ff319278335
Shachar <110262023+shacharrr@users.noreply.github.com>
2023-07-17 20:38:17 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,84 +2,145 @@
 
 
 use mki::Keyboard;
-use std::thread;
+use std::{thread, io::Write};
 use std::time::Duration;
-
-use ini::Ini;
-use eframe::egui;
-use egui::{menu, Visuals};
+use serde::{Serialize, Deserialize};
+use std::fs;
 use std::collections::HashMap;
 
+use eframe::egui;
+use egui::{menu, Visuals};
 
-const CFG_PATH: &str = "./cfg/cjump.ini";
-static mut DARK: bool = true;
+
+const CFG_PATH: &str = "./cfg/cjump.toml";
 // Shity way should improve
 static mut FLOOP: bool = true;
-
-static mut JUMP_BIND: Keyboard = Keyboard::Space;
-static mut PAUSE_BIND: Keyboard = Keyboard::Enter;
 static mut PAUSED: bool = false;
-static mut DELAY: u64 = 850;
+
+static mut TOML_CONVAR: TomlConVar = TomlConVar::new();
+static mut CONVAR: ConVar =  ConVar::new();
+
+#[derive(Deserialize, Serialize)]
+struct TomlConVar {
+    dark_mode: String,
+    config: TomlConfig,
+}
+
+impl TomlConVar {
+    const fn new() -> TomlConVar {
+        TomlConVar { 
+            dark_mode: String::new(),
+            config: TomlConfig {
+                jump_bind: String::new(),
+                duck_bind: String::new(),
+                pause_bind: String::new(),
+                delay: String::new(),
+            }
+        }
+    }
+
+    fn convert_to_convar(&mut self) -> ConVar {
+        ConVar {
+            dark_mode: self.dark_mode.parse().unwrap(),
+            config: Config {
+                jump_bind: self.config.jump_bind.clone(),
+                duck_bind: self.config.duck_bind.clone(),
+                pause_bind: self.config.pause_bind.clone(),
+                delay: self.config.delay.parse().unwrap(),
+            }
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct TomlConfig {
+    jump_bind: String,
+    duck_bind: String,
+    pause_bind: String,
+    delay: String,
+}
+
+struct ConVar {
+    dark_mode: u8,
+    config: Config,
+}
+
+impl ConVar {
+    const fn new() -> ConVar {
+        ConVar { 
+            dark_mode: 1,
+            config: Config {
+                jump_bind: String::new(),
+                duck_bind: String::new(),
+                pause_bind: String::new(),
+                delay: 850,
+            }
+        }
+    }
+
+    fn convert_to_toml(&mut self) -> TomlConVar {
+        TomlConVar {
+            dark_mode: self.dark_mode.to_string(),
+            config: TomlConfig {
+                jump_bind: self.config.jump_bind.clone(),
+                duck_bind: self.config.duck_bind.clone(),
+                pause_bind: self.config.pause_bind.clone(),
+                delay: self.config.delay.to_string(),
+            }
+        }
+    }
+}
+
+struct Config {
+    jump_bind: String,
+    duck_bind: String,
+    pause_bind: String,
+    delay: u64,
+}
+
 
 fn main() -> Result<(), eframe::Error> {
     match std::fs::metadata(CFG_PATH) {
         Ok(_) => {
-            let mut cfg = Ini::load_from_file(CFG_PATH).unwrap();
-            let mut s = cfg.with_section(Some("style"));
-            let d = s.get("dark").unwrap();
-            if d == "0"{
-                unsafe { DARK = false };
+            let contents = fs::read_to_string(CFG_PATH)
+            .expect("Should have been able to read the file");
+            
+            unsafe {
+                TOML_CONVAR = toml::from_str(contents.as_str()).unwrap();
+                CONVAR = TOML_CONVAR.convert_to_convar();
             }
-
-            let jump_vk = create_jump_hash();
-            let mut s = cfg.with_section(Some("binds"));
-            let d = s.get("jump").unwrap();
-            unsafe {
-                JUMP_BIND = jump_vk[d];
-            };
-
-            let pause_vk = create_pause_hash();
-            let mut s = cfg.with_section(Some("binds"));
-            let d = s.get("pause").unwrap();
-            unsafe {
-                PAUSE_BIND = pause_vk[d];
-            };
-
-            let mut s = cfg.with_section(Some("binds"));
-            let d = s.get("delay").unwrap();
-            unsafe {
-                DELAY = d.parse().unwrap();
-            };
         },
         Err(_) => {
             let path = std::path::Path::new(CFG_PATH);
             let prefix = path.parent().unwrap();
             std::fs::create_dir_all(prefix).unwrap();
-            std::fs::File::create(path).unwrap();
+            let mut f = std::fs::File::create(path).unwrap();
 
-            // Ini init
-            let mut cfg = Ini::new();
-            cfg.with_section(Some("style"))
-                .set("dark", "1");
-            cfg.with_section(Some("binds"))
-                .set("jump", "Space");
-            cfg.with_section(Some("binds"))
-                .set("pause", "Enter");
-            cfg.with_section(Some("binds"))
-                .set("delay", "850");
+            let default_toml = r#"dark_mode = '1'
 
-            cfg.write_to_file(CFG_PATH).unwrap();
+[config]
+jump_bind = 'Space'
+duck_bind = 'Left Control'
+pause_bind = 'Enter'
+delay = '850'
+            "#;
 
+            let _ = f.write_all(default_toml.as_bytes());
+            
             unsafe {
-                DARK = true;
+                TOML_CONVAR = toml::from_str(default_toml).unwrap();
+                CONVAR = TOML_CONVAR.convert_to_convar();
             };
         },
     }
 
     thread::spawn(|| {
+        let jump_vk = create_jump_hash();
+        let duck_vk = create_duck_hash();
+        let pause_vk = create_pause_hash();
         loop {
             let mut spfloop = true;
-            while unsafe { PAUSE_BIND.is_pressed() } {
+            while unsafe { pause_vk[&CONVAR.config.pause_bind].is_pressed() } {
                 if spfloop {
                     unsafe { PAUSED = !PAUSED };
                     spfloop = false;
@@ -87,12 +148,12 @@ fn main() -> Result<(), eframe::Error> {
             }
 
             let mut sjfloop = true;
-            while unsafe { JUMP_BIND.is_pressed() && !PAUSED} {
+            while unsafe { jump_vk[&CONVAR.config.jump_bind].is_pressed() && !PAUSED} {
                 if sjfloop {
                     sjfloop = false;
-                    Keyboard::LeftControl.press();
-                    unsafe { thread::sleep(Duration::from_millis(DELAY)) }
-                    Keyboard::LeftControl.release();
+                    unsafe { duck_vk[&CONVAR.config.duck_bind].press(); }
+                    unsafe { thread::sleep(Duration::from_millis(CONVAR.config.delay)) }
+                    unsafe { duck_vk[&CONVAR.config.duck_bind].release(); }
                 }
             }
         }
@@ -109,20 +170,11 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
-fn find_key_for_value<'a>(map: &'a HashMap<String, Keyboard>, value: Keyboard) -> Option<&'a String> {
-    map.iter()
-        .find_map(|(key, &val)| if val == value { Some(key) } else { None })
-}
-
 fn create_jump_hash() -> HashMap<String, Keyboard> {
     let mut jump_vk = HashMap::new();
     jump_vk.insert(
         "Space".to_string(),
         Keyboard::Space,
-    );
-    jump_vk.insert(
-        "C".to_string(),
-        Keyboard::C,
     );
     jump_vk.insert(
         "V".to_string(),
@@ -135,6 +187,19 @@ fn create_jump_hash() -> HashMap<String, Keyboard> {
     jump_vk.insert(
         "N".to_string(),
         Keyboard::N,
+    );
+    jump_vk
+}
+
+fn create_duck_hash() -> HashMap<String, Keyboard> {
+    let mut jump_vk = HashMap::new();
+    jump_vk.insert(
+        "Left Control".to_string(),
+        Keyboard::LeftControl,
+    );
+    jump_vk.insert(
+        "C".to_string(),
+        Keyboard::C,
     );
     jump_vk
 }
@@ -210,28 +275,13 @@ fn create_pause_hash() -> HashMap<String, Keyboard> {
 
 
 struct MyApp {
-    dark: bool,
-    jump_vk: HashMap<String, Keyboard>,
-    jump_select: String,
-    pause_vk: HashMap<String, Keyboard>,
-    pause_select: String,
-    delay: u64
+    dark: u8,
 }
-
 
 impl Default for MyApp {
     fn default() -> Self {
-        let jump_vk = create_jump_hash();
-        let pause_vk = create_pause_hash();
-        let jc = jump_vk.clone();
-        let cp = pause_vk.clone();
         Self {
-            dark: unsafe { DARK },
-            jump_vk: jump_vk,
-            jump_select: find_key_for_value(&jc, unsafe { JUMP_BIND }).unwrap().to_string(),
-            pause_vk: pause_vk,
-            pause_select: find_key_for_value(&cp, unsafe { PAUSE_BIND }).unwrap().to_string(),
-            delay: unsafe { DELAY },
+            dark: unsafe { CONVAR.dark_mode },
         }
     }
 }
@@ -241,27 +291,29 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             
             // Light/Dark Mode
-            if self.dark != unsafe{DARK} || unsafe{FLOOP} {
-                let mut cfg = Ini::load_from_file(CFG_PATH).unwrap();
-                cfg.with_section(Some("style"))
-                    .set("dark", (self.dark as u8).to_string().as_str());
-                cfg.write_to_file(CFG_PATH).unwrap();
-
+            if self.dark != unsafe{ CONVAR.dark_mode } || unsafe{FLOOP} {
                 unsafe {
-                    DARK = self.dark;
+                    CONVAR.dark_mode = self.dark;
                     FLOOP = false;
                 };
 
                 match self.dark {
-                    false => ctx.set_visuals(Visuals::light()),
-                    true => ctx.set_visuals(Visuals::dark()),
+                    1 => ctx.set_visuals(Visuals::light()),
+                    0 => ctx.set_visuals(Visuals::dark()),
+                    _ => todo!(),
                 }
             }
             // Menu bar
             menu::bar(ui, |ui| {
                 ui.menu_button("Visuals", |ui| {
-                    ui.checkbox(&mut self.dark, "Dark mode");
-                })
+                    if ui.button("Dark mode").clicked() {
+                        match self.dark {
+                            1 => self.dark = 0,
+                            0 => self.dark = 1,
+                            _ => todo!()
+                        }
+                    }
+                });
             });
 
             ui.horizontal(|ui| {
@@ -276,35 +328,42 @@ impl eframe::App for MyApp {
             ui.separator();
 
             egui::ComboBox::from_label("Jump bind")
-                .selected_text(format!("{:?}", self.jump_select))
+                .selected_text(format!("{:?}", unsafe { &CONVAR.config.jump_bind }))
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(&mut self.jump_select, String::from("Space"), "Space");
-                    ui.selectable_value(&mut self.jump_select, String::from("C"), "C");
-                    ui.selectable_value(&mut self.jump_select, String::from("V"), "V");
-                    ui.selectable_value(&mut self.jump_select, String::from("B"), "B");
-                    ui.selectable_value(&mut self.jump_select, String::from("N"), "N");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.jump_bind }, String::from("Space"), "Space");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.jump_bind }, String::from("V"), "V");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.jump_bind }, String::from("B"), "B");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.jump_bind }, String::from("N"), "N");
+                }
+            );
+
+            egui::ComboBox::from_label("Duck bind")
+                .selected_text(format!("{:?}", unsafe { &CONVAR.config.duck_bind }))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.duck_bind }, String::from("Left Control"), "L-CTRL");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.duck_bind }, String::from("C"), "C");
                 }
             );
 
             egui::ComboBox::from_label("Pause bind")
-                .selected_text(format!("{:?}", self.pause_select))
+                .selected_text(format!("{:?}", unsafe{ &CONVAR.config.pause_bind }))
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(&mut self.pause_select, String::from("Enter"), "Enter");
-                    ui.selectable_value(&mut self.pause_select, String::from("Tab"), "Tab");
-                    ui.selectable_value(&mut self.pause_select, String::from("Backspace"), "Backspace");
-                    ui.selectable_value(&mut self.pause_select, String::from("Left Alt"), "Left Alt");
-                    ui.selectable_value(&mut self.pause_select, String::from("F1"), "F1");
-                    ui.selectable_value(&mut self.pause_select, String::from("F2"), "F2");
-                    ui.selectable_value(&mut self.pause_select, String::from("F3"), "F3");
-                    ui.selectable_value(&mut self.pause_select, String::from("F4"), "F4");
-                    ui.selectable_value(&mut self.pause_select, String::from("F5"), "F5");
-                    ui.selectable_value(&mut self.pause_select, String::from("F6"), "F6");
-                    ui.selectable_value(&mut self.pause_select, String::from("F7"), "F7");
-                    ui.selectable_value(&mut self.pause_select, String::from("F8"), "F8");
-                    ui.selectable_value(&mut self.pause_select, String::from("F9"), "F9");
-                    ui.selectable_value(&mut self.pause_select, String::from("F10"), "F10");
-                    ui.selectable_value(&mut self.pause_select, String::from("F11"), "F11");
-                    ui.selectable_value(&mut self.pause_select, String::from("F12"), "F12");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("Enter"), "Enter");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("Tab"), "Tab");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("Backspace"), "Backspace");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("Left Alt"), "L-ALT");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F1"), "F1");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F2"), "F2");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F3"), "F3");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F4"), "F4");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F5"), "F5");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F6"), "F6");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F7"), "F7");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F8"), "F8");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F9"), "F9");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F10"), "F10");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F11"), "F11");
+                    ui.selectable_value(unsafe{ &mut CONVAR.config.pause_bind }, String::from("F12"), "F12");
                 }
             );
 
@@ -312,24 +371,17 @@ impl eframe::App for MyApp {
             
             ui.horizontal(|ui| {
                 ui.label("Delay (ms)");
-                ui.add(egui::DragValue::new(&mut self.delay).speed(1));
+                ui.add(egui::DragValue::new(unsafe { &mut CONVAR.config.delay }).speed(1));
             });
 
             if ui.button("Update settings").clicked() {
-                let mut cfg = Ini::load_from_file(CFG_PATH).unwrap();
-                cfg.with_section(Some("binds"))
-                    .set("jump", self.jump_select.as_str());
-                cfg.with_section(Some("binds"))
-                    .set("pause", self.pause_select.as_str());
-                cfg.with_section(Some("binds"))
-                    .set("delay", self.delay.to_string());
-
-                cfg.write_to_file(CFG_PATH).unwrap();
+                let path = std::path::Path::new(CFG_PATH);
+                let mut f = std::fs::File::create(path).unwrap();
 
                 unsafe {
-                    JUMP_BIND = self.jump_vk[self.jump_select.as_str()];
-                    PAUSE_BIND = self.pause_vk[self.pause_select.as_str()];
-                    DELAY = self.delay;
+                    TOML_CONVAR = CONVAR.convert_to_toml();
+                    let content = toml::to_string(&TOML_CONVAR).unwrap();
+                    let _ = f.write_all(content.as_bytes());
                 };
             }
         });
